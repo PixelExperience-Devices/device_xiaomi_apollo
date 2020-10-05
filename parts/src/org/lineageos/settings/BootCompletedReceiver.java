6e101e20475001c21ca4f89fd9fe09d3094741fa
dataoutputstream <mb150996@gmail.com>
2020-10-05 13:25:27 +0200

    apollo: parts: Add touchfeature impl of dt2w
    Change-Id: I81935db0a1ee911cfd9cd890fb40c1f9aaaf441d

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,8 +20,10 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 import org.lineageos.settings.RefreshRateHandler;
+import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
 
 import org.lineageos.settings.doze.DozeUtils;
 
@@ -30,12 +32,22 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
 
+    public static final String SHAREDD2TW = "sharadeD2TW";
+    private ITouchFeature mTouchFeature;
+
     @Override
     public void onReceive(final Context context, Intent intent) {
     RefreshRateHandler.setFPS(RefreshRateHandler.getRefreshRate(context));
+    //Micro-Service to restore sata of dt2w on reboot
+    SharedPreferences prefs = context.getSharedPreferences(SHAREDD2TW, Context.MODE_PRIVATE);
+    try {
+        mTouchFeature = ITouchFeature.getService();
+        mTouchFeature.setTouchMode(14,prefs.getInt(SHAREDD2TW, 1));
+    } catch (Exception e) {
+        // Do nothing
+    }
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
     }
 
 }
-
