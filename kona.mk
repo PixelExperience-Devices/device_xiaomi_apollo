b734bc60321bc2b87c4083ef3c3cd8b6192a4bb6
Zinadin Zidan <zidan44@pixelexperience.org>
2021-03-13 12:06:21 +0600

    sm8250-common: Drop lineage livedisplay
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Id5f8f1dc01cff3f308f3ad3602fd163f29461580

--- kona.mk
+++ kona.mk
@@ -247,10 +247,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.kona
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
