7b0be65abe122a0294e4663da241d225a2941ee8
Zinadin Zidan <zidan44@pixelexperience.org>
2021-03-19 21:21:35 +0600

    apollo: Build power stats hardware service mock
    * E BatteryStatsService: Unable to load Power Hal or power.stats HAL
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Id7ae9cbc563fad84d45706cbff85a9b9945a1680

--- device.mk
+++ device.mk
@@ -285,7 +285,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Public libraries
 PRODUCT_COPY_FILES += \
