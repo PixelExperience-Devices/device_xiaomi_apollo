83b6c1a4dc63c9221eebe708e395ca6cddfda320
Zinadin Zidan <zidan44@pixelexperience.org>
2021-04-04 20:48:26 +0600

    sm8250-common: fod: Drop FOD implementation
    * This commit revert of https://github.com/PixelExperience-Devices/device_xiaomi_apollo/commit/b4c826232cbeabf903fb41e9ca63e6c8f1b8a54d
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I302d3731069e8007a33095c74648e5d3d88dc4e5

--- kona.mk
+++ kona.mk
@@ -137,11 +137,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
-ifneq ($(TARGET_NO_FOD),true)
-PRODUCT_PRODUCT_PROPERTIES += \
-    doze.enable_fod_service=true
-endif
-
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
@@ -180,12 +175,8 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
-ifeq ($(TARGET_NO_FOD),true)
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm8250
-endif
-
-PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm8250 \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
 
 # GPS
