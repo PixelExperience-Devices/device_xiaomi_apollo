33d0db9f831487f12858257f8a4c5bf8fb0cc543
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-04-21 22:15:07 +0200

    apollo: Use QTI USB init configs from USB HAL repo
    Change-Id: I966d251a2756c9d9e91f621cbc5872448d022de4

--- device.mk
+++ device.mk
@@ -338,7 +338,6 @@ PRODUCT_PACKAGES += \
     init.qcom.sdio.sh \
     init.qcom.sensors.sh \
     init.qcom.sh \
-    init.qcom.usb.sh \
     init.qti.dcvs.sh \
     init.qti.media.sh \
     qca6234-service.sh \
@@ -348,7 +347,6 @@ PRODUCT_PACKAGES += \
     init.qcom.factory.rc \
     init.qcom.power.rc \
     init.qcom.rc \
-    init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     fstab.qcom \
@@ -399,6 +397,13 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.2-service-qti
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 # Vibrator
 PRODUCT_PACKAGES += \
