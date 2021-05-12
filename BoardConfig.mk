fb2e1e07ec43e41f06cdc56ee23c32e34143154f
Zinadin Zidan <zidan44@pixelexperience.org>
2021-05-13 04:21:41 +0600

    apollo: kernel: Switch to new oss kernel
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I7e0700cc11dca257913b64037a7234bdb69c36b9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,7 +84,34 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollo
 TARGET_RECOVERY_DEVICE_MODULES := libinit_apollo
 
 # Kernel
-include device/xiaomi/apollo-kernel/BoardConfigKernel.mk
+BOARD_BOOT_HEADER_VERSION := 2
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_RAMDISK_OFFSET := 0x02000000
+
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.memcg=1
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += service_locator.enable=1
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += swiotlb=2048
+BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem,nosocket
+BOARD_KERNEL_CMDLINE += reboot=panic_warm
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+TARGET_KERNEL_CLANG_COMPILE := true
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+TARGET_KERNEL_SOURCE := kernel/xiaomi/apollo
+TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
+
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 # Light
 TARGET_PROVIDES_LIBLIGHT := true
