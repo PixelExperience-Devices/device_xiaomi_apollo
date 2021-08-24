90a3f0ab32fc196637b8e381c446d933e84eeda4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-24 16:36:22 +0200

    apollo: libinit: Adapt to latest changes
    Change-Id: I9a2cacdef922f19b788316537ef15394e5068edb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,8 +15,8 @@ BUILD_BROKEN_DUP_RULES := true
 TARGET_SCREEN_DENSITY := 440
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollo
-TARGET_RECOVERY_DEVICE_MODULES := libinit_apollo
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_apollo
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_apollo
 
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
