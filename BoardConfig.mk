92ae03f26b9cafbb75c39dc0107148005c0f129f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-15 12:06:47 +0200

    apollo: Move TARGET_VENDOR_PROP to BoardConfig.mk
    Change-Id: I1ab83ebedff7c58776dc5e891d2bf8bd8eb56058

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,5 +24,8 @@ TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := apollo,apollopro
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Inherit from the proprietary version
 include vendor/xiaomi/apollo/BoardConfigVendor.mk
