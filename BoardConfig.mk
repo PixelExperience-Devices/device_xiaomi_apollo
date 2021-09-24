590052e79dbf8dd842aa206a7d63182ebe4bd044
Zinadin Zidan <zidan.roking@gmail.com>
2021-09-24 19:16:31 +0600

    apollo: Switch to new IMMENSITY-X kernel
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I8cd233ae232e1d6bcba32ec5747b852753be4edf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_apollo
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_apollo
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
+TARGET_KERNEL_CONFIG := apollo_defconfig
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := apollo,apollopro
